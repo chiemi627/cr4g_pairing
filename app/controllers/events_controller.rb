61afba44588dda9433462b8f637190194aac9641
Chiemi Watanabe <chiemi@a.tsukuba-tech.ac.jp>
2019-07-08 13:21:54 +0900

    イベント作成フォームをちょっと整えた

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,9 +4,13 @@ class EventsController < ApplicationController
 
   def create
     event = Event.new(event_params)
-    event.save
-    flash[:success] = "#{event.name}を登録しました。"+root_url(only_path: false) + "events/" + event.account + " でペアリングが自動生成されます。"
-    redirect_to controller: 'pairings', action: 'index', account: event.account
+    if  event.save
+      flash[:success] = "#{event.name}を登録しました。"+root_url(only_path: false) + "events/" + event.account + " でペアリングが自動生成されます。"
+      redirect_to controller: 'pairings', action: 'index', account: event.account
+    else
+      flash[:danger] = "登録できませんでした。"+event.errors.full_messages.join(", ")
+      redirect_to action: 'new'
+    end
   end
 
   def event_params
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,9 @@
 class Event < ApplicationRecord
+    validates :account,
+    presence: true,
+    uniqueness: true,
+    length: {maximum: 16},
+    format: {with: /\A[a-z0-9]+\z/i }
+    validates :googlesheet,
+    presence: true
 end
