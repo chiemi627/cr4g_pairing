527683c0fbdd88348d13aee4311ff52782da1e90
Chiemi Watanabe <chiemi@slis.tsukuba.ac.jp>
2019-07-19 18:47:18 +0900

    model 過去のパートナーを求めるメソッドを作った

--- app/models/pairing_log.rb
+++ app/models/pairing_log.rb
@@ -2,4 +2,18 @@ class PairingLog < ApplicationRecord
     validates :name,
     length: {maximum: 50},
     format: {with: /\A[a-z0-9]+\z/i }
+
+    def past_partners(event,id)
+        partners = []
+        PairingLog.where(event: event).each { |log|
+            data = JSON.parse(log.data).with_indifferent_access
+            data[:pairs].each{ |pair|
+                if pair.include?(id) 
+                    pair.delete(id) 
+                    partners.push(pair[0])
+                end
+            }
+        }
+        partners
+    end
 end
--- test/models/pairing_log_test.rb
+++ test/models/pairing_log_test.rb
@@ -1,16 +1,19 @@
 require 'test_helper'
 
 class PairingLogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
-  # 同じイベントのセッションリストが出力できる
-  # test イベントcr４gのセッションリストを求めるとs1とs2が返る
 
   test "イベントcr4gのセッションリストを求めるとs1とs2が返る" do
     sessions = PairingLog.where(event: "cr4g").pluck('name')
     assert_includes(sessions,"s1") 
     assert_includes(sessions,"s2") 
   end
+
+  # s1: [1,3],[2,4]でs2: [1,4],[2,3]だとすると
+  test "イベントcr4gで1がペアを組んだのは3と4" do
+    log = PairingLog.new
+    partners = log.past_partners("cr4g",1)
+    assert_includes(partners,3)
+    assert_includes(partners,4)
+  end
+
 end
